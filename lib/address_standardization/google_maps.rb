6e5b1f082083a9444c671cd6193eca1a582b806c
Elliot Winkler <elliot.winkler@gmail.com>
2009-01-22 14:58:00 -0600

    When passing data to Google Maps, do not separate pieces with commas; also return nil if we find an address but the street or city is blank

--- lib/address_standardization/google_maps.rb
+++ lib/address_standardization/google_maps.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'hpricot'
 
 module AddressStandardization
+  # See <http://code.google.com/apis/maps/documentation/geocoding/>
   module GoogleMaps
     class << self
       attr_accessor :api_key
@@ -15,18 +16,18 @@ module AddressStandardization
         def standardize(address_info)
           raise "API key not specified.\nCall AddressStandardization::GoogleMaps.api_key = '...' before you call .standardize()." unless GoogleMaps.api_key
           
-          address_str = "%s, %s, %s %s" % [
+          address_str = [
             address_info[:street],
             address_info[:city],
             (address_info[:state] || address_info[:province]),
             address_info[:zip]
-          ]
+          ].join(" ")
           url = "http://maps.google.com/maps/geo?q=#{address_str.url_escape}&output=xml&key=#{GoogleMaps.api_key}&oe=utf-8"
-          # puts url
+          puts "Calling Google Maps URL: #{url}"
           uri = URI.parse(url)
           res = Net::HTTP.get_response(uri)
           unless res.is_a?(Net::HTTPSuccess)
-            File.open("test.xml", "w") {|f| f.write("(no response or response was unsuccessful)") }
+            #File.open("test.xml", "w") {|f| f.write("(no response or response was unsuccessful)") }
             return nil 
           end
           xml = res.body
@@ -36,11 +37,13 @@ module AddressStandardization
           if xml.at("//kml/Response/Status/code").inner_text == "200"
             addr = {}
             
-            addr[:street] = get_inner_text(xml, '//ThoroughfareName')
-            addr[:city] = get_inner_text(xml, '//LocalityName')
-            addr[:province] = addr[:state] = get_inner_text(xml, '//AdministrativeAreaName')
-            addr[:zip] = addr[:postalcode] = get_inner_text(xml, '//PostalCodeNumber')
-            addr[:country] = get_inner_text(xml, '//CountryName')
+            addr[:street]   = get_inner_text(xml, '//ThoroughfareName').to_s
+            addr[:city]     = get_inner_text(xml, '//LocalityName').to_s
+            addr[:province] = addr[:state] = get_inner_text(xml, '//AdministrativeAreaName').to_s
+            addr[:zip]      = addr[:postalcode] = get_inner_text(xml, '//PostalCodeNumber').to_s
+            addr[:country]  = get_inner_text(xml, '//CountryName').to_s
+            
+            return nil if addr[:street] =~ /^\s*$/ or addr[:city]  =~ /^\s*$/
             
             new(addr)
           else
